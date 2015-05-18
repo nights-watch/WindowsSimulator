a7e77e42ae9d990db238ba0e600966d207b2668b
marcos <marcos.rpj@hotmail.com>
2015-05-18 12:05:02 -0300

    fix resolution problems.

--- src/br/unb/saltar/robot/simulations/BrowserSimulator.java
+++ src/br/unb/saltar/robot/simulations/BrowserSimulator.java
@@ -14,21 +14,21 @@ public class BrowserSimulator extends Simulator {
 
 	@Override
 	protected void setRobotBehavior() {
-		addCommand(new OpenURICommand("www.outlook.com"), 5000);
-		addCommand(new TypeCommand("lades.fga@outlook.com\n"), 1000);
-		addCommand(new TypeCommand("L@desFG@\n"), 4000);
+		addCommand(new OpenURICommand("www.outlook.com"), 15000);
+		addCommand(new TypeCommand("lades.fga@outlook.com\n"), 3000);
+		addCommand(new TypeCommand("L@desFG@\n"), 15000);
+		addCommand(new AbsoluteClickCommand(300, 100), 15000);
+		addCommand(new TypeCommand("lades.fga@outlook.com\t"), 3000);
+		addCommand(new TypeCommand("\tEmail novo de Lades!!"), 3000);
+		addCommand(new TypeCommand("\tSegredo do Lades!"), 3000);
 		addCommand(new AbsoluteClickCommand(300, 100), 10000);
-		addCommand(new TypeCommand("lades.fga@outlook.com\t"), 1000);
-		addCommand(new TypeCommand("\tEmail novo de Lades!!"), 1000);
-		addCommand(new TypeCommand("\tSegredo do Lades!"), 2000);
-		addCommand(new AbsoluteClickCommand(300, 100), 2000);
 	}
 
 	@Override
 	protected void setGoBackBehavior() {
-		addCommand(new AbsoluteClickCommand(1250, 100), 2000);
-		addCommand(new AbsoluteClickCommand(1250, 300), 10000);
-		addCommand(new AbsoluteClickCommand(1360, 5), 2000);
+		addCommand(new AbsoluteClickCommand(950, 100), 3000);
+		addCommand(new AbsoluteClickCommand(950, 300), 15000);
+		addCommand(new AbsoluteClickCommand(1000, 5), 100);
 
 	}
 
